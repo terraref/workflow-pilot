0166a393d066fbd1842d9a05e2532204a4214ff7
Max Burnette <mburnet2@illinois.com>
2018-09-05 14:22:21 -0500

    add job running stuff

---
+++ workflow_terra/submit.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+set -e
+
+TOP_DIR=`dirname $0`
+TOP_DIR=`cd $TOP_DIR/.. && pwd`
+cd $TOP_DIR
+
+# exec env
+EXEC_ENV="condor_pool"
+case $EXEC_ENV in
+    "condor_pool")
+        EXEC_SITE="condor_pool"
+        STAGING_SITE="local"
+        OUTPUT_SITE="local"
+        ;;
+    "psc_bridges")
+        EXEC_SITE="condor_pool"
+        STAGING_SITE="local"
+        OUTPUT_SITE="local"
+        ;;
+    "isi_shared")
+        EXEC_SITE="isi_shared"
+        STAGING_SITE="isi_shared"
+        OUTPUT_SITE="isi_shared"
+        ;;
+    *)
+        echo "Unknown execution environment."
+        exit 1
+        ;;
+esac
+
+export RUN_ID=stereo_rgb-`date +'%s'`
+export RUN_DIR=$HOME/workflows/$RUN_ID
+
+mkdir -p $RUN_DIR
+mkdir -p workflow/generated
+
+# create a site catalog from the template
+envsubst < sites.template.xml > workflow/generated/sites.xml
+
+# generate a transformation catalog
+./tc-generator.sh $EXEC_ENV >workflow/generated/tc.data
+
+# generate the workflow
+#./workflow_generator.py
+
+# plan and submit
+pegasus-plan \
+    --conf pegasus.conf \
+    --sites $EXEC_SITE \
+    --staging-site $STAGING_SITE \
+    --output-site $OUTPUT_SITE \
+    --cluster horizontal \
+    --cleanup leaf \
+    --relative-dir $RUN_ID \
+    --dir $RUN_DIR \
+    --dax workflow/generated/2018-07-01__stereovis_ir_sensors_partialplots_sorghum6_shade_flir_eastedge_mn_14140c8a-e13a-4ccd-a120-d3dbf3eacef4.xml \
+    --submit 
+
+
---
+++ workflow_terra/tc-generator.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+if [ "X$1" == "Xcondor_pool" ]; then
+
+    cat <<EOF
+
+cont terraref {
+    type "singularity"
+    image "http://workflow.isi.edu/TERRAREF/terraref.img"
+}
+
+tr bin2tif.sh {
+    site local {
+        type "STAGEABLE"
+        container "terraref"
+        pfn "file://$PWD/wrappers/bin2tif.sh"
+    }
+}
+
+tr nrmac.sh {
+    site local {
+        type "STAGEABLE"
+        container "terraref"
+        pfn "file://$PWD/wrappers/nrmac.sh"
+    }
+}
+
+tr merge.sh {
+    site local {
+        type "STAGEABLE"
+        container "terraref"
+        pfn "file://$PWD/wrappers/merge.sh"
+    }
+}
+
+tr fieldmosaic.sh {
+    site local {
+        type "STAGEABLE"
+        container "terraref"
+        pfn "file://$PWD/wrappers/fieldmosaic.sh"
+    }
+}
+
+tr canopy_cover.sh {
+    site local {
+        type "STAGEABLE"
+        container "terraref"
+        pfn "file://$PWD/wrappers/canopy_cover.sh"
+    }
+}
+
+EOF
+
+else
+
+    cat <<EOF
+
+tr bin2tif.sh {
+    site isi_shared {
+        type "INSTALLED"
+        pfn "/lizard/projects/terraref/bin/bin2tif.sh"
+    }
+}
+
+tr merge.sh {
+    site isi_shared {
+        type "INSTALLED"
+        pfn "/lizard/projects/terraref/bin/merge.sh"
+    }
+}
+
+tr fieldmosaic.sh {
+    site isi_shared {
+        type "INSTALLED"
+        pfn "/lizard/projects/terraref/bin/fieldmosaic.sh"
+    }
+}
+
+tr canopy_cover.sh {
+    site isi_shared {
+        type "INSTALLED"
+        pfn "/lizard/projects/terraref/bin/canopy_cover.sh"
+    }
+}
+
+EOF
+
+fi
+
+
