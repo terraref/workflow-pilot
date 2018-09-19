e87ffbc4dae93b8238228eeffec4445a5b38f2c7
Max Burnette <mburnet2@illinois.com>
2018-09-19 14:11:09 -0500

    remove uiuc_shared

--- workflow_terra/submit.sh
+++ workflow_terra/submit.sh
@@ -24,11 +24,6 @@ case $EXEC_ENV in
         STAGING_SITE="isi_shared"
         OUTPUT_SITE="isi_shared"
         ;;
-    "uiuc_shared")
-        EXEC_SITE="uiuc_shared"
-        STAGING_SITE="uiuc_shared"
-        OUTPUT_SITE="uiuc_shared"
-        ;;
     *)
         echo "Unknown execution environment."
         exit 1
