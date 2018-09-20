81d7446dc0c2b5eb5e203347f40a0f1daa0de798
Max Burnette <mburnet2@illinois.com>
2018-09-20 10:32:35 -0500

    add echos to submit.sh

--- workflow_terra/submit.sh
+++ workflow_terra/submit.sh
@@ -33,19 +33,24 @@ esac
 export RUN_ID=stereo_rgb-`date +'%s'`
 export RUN_DIR=$HOME/workflows/$RUN_ID
 
+echo "Creating $RUN_DIR"
 mkdir -p $RUN_DIR
 mkdir -p workflow/generated
 
 # create a site catalog from the template
+echo "Generating site catalog"
 envsubst < sites.template.xml > workflow/generated/sites.xml
 
 # generate a transformation catalog
+echo "Generating transformation catalog"
 ./tc-generator.sh $EXEC_ENV >workflow/generated/tc.data
 
 # generate the workflow
+echo "Calling workflow_generator"
 ./workflow_generator.py
 
 # plan and submit
+echo "Submitting singletest.xml"
 pegasus-plan \
     --conf pegasus.conf \
     --sites $EXEC_SITE \
@@ -56,6 +61,4 @@ pegasus-plan \
     --relative-dir $RUN_ID \
     --dir $RUN_DIR \
     --dax workflow/generated/singletest.xml \
-    --submit 
-
-
+    --submit
