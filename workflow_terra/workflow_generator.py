44711b4743ffe7397008d335bba56aa4345aab98
Max Burnette <mburnet2@illinois.com>
2018-10-09 11:56:15 -0500

    add path replacement back in

--- workflow_terra/workflow_generator.py
+++ workflow_terra/workflow_generator.py
@@ -360,8 +360,8 @@ def create_scan_dax(date, scan_name, scan_list, tools):
     field_paths_qual_daxf = create_daxf(field_paths_qual, os.path.join(root_dir, fullfield_out_dir, field_paths_qual), dax)
     with open(os.path.join(root_dir, fullfield_out_dir, field_paths_qual), 'w') as j:
         for path in fieldmosaic_quality_inputs:
-            #j.write("%s\n" % re.sub(r'/', '___', path))
-            j.write("%s\n" % os.path.basename(path))
+            j.write("%s\n" % re.sub(r'/', '___', path))
+            #j.write("%s\n" % os.path.basename(path))
 
     # OUTPUT
     # when running in condorio mode, lfns are flat, so create a tarball with the deep lfns for the fieldmosaic
